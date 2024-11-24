ea0f9ca4c125c8f5296c4d27e2c1745b44174088
kholoudSal202 <kholoudsalman2003@gmail.com>
2024-11-24 12:31:09 +0300

    change
    myExam

---
+++ loop/Program.cs
@@ -0,0 +1,165 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;  
+using System.Threading.Tasks;   
+using System.Windows;
+public class Kholoud
+{
+    public static void Main(String[] args)
+    {
+        string Name="kholoud";
+        int Age;
+        double hourly_Wage;
+
+        Console.WriteLine("Hi, my name is John. I am 30 years old and earn $20.5 per hour.");
+        Console.WriteLine($"Hi, my name is {Name} I am {21} years old and earn {100.50} per hour.");
+
+int[] ints= new int[5] { 1,2,4,6,8};
+Console.WriteLine("array"+ints);
+    
+public void ArraySum(){
+    for (MyArray){
+        MyArray.Add([i]);
+        
+
+    }
+}
+ int[] MyArray = new int[5] {0,0,0,0,0};
+Console.WriteLine("fill the array with 5 values");
+// int ArraySize=5;
+// while(true){
+// Console.ReadLine();
+// }
+// if (MyArray.Length != 5){}
+foreach(int i in MyArray){
+    int[MyArray] = i;
+    Console.WriteLine(i);
+   
+    
+ }
+ 
+for (int i = 1;i<=20;i++){
+    Console.WriteLine(i);
+
+}
+
+int i=1;
+while (i<=20 ){
+Console.WriteLine(i)
+   i++;
+    }
+
+
+
+public Car = new C1(string Make, string Model, int Year,string Color){
+   Make ="saudi";
+   Model = "bmw";
+   Year = 2021; 
+   Color = "gray";
+    DisplayDetails();
+
+}
+
+public Car = new C2(string Make, string Model, int Year,string Color){
+   Make ="japan";
+   Model = "kia";
+   Year = 2022; 
+   Color = "green";
+    DisplayDetails();
+
+}
+
+public Car = new C3(string Make, string Model, int Year,string Color){
+   Make ="ford";
+   Model = "bmw";
+   Year = 2012; 
+   Color = "blue";
+    DisplayDetails();
+
+}
+
+
+
+
+
+
+
+
+// int[0]=Console.ReadLine();
+// Console.WriteLine("enter first value");
+// int[1]=Console.ReadLine();
+// Console.WriteLine("enter secened value");
+// Console.ReadLine();
+// Console.WriteLine("enter thired value");
+// Console.ReadLine();
+// Console.WriteLine("enter forth value");
+// Console.ReadLine();
+// Console.WriteLine("enter fifth value");
+// Console.ReadLine();
+// Console.WriteLine(MyArray);
+// Console.WriteLine("");
+
+Console.WriteLine("what`s Your Name?");
+GetGreeting();
+
+
+Console.WriteLine("enter a number");
+Console.ReadLine();
+
+ int Number;
+int result;
+if(Number>0){
+
+    Console.WriteLine("it`s Positive");
+ Number= result;
+ Console.WriteLine(result);
+} else if(Number<0){
+    Console.WriteLine("it`s Negative");
+    Number= result;
+     Console.WriteLine(result);
+}else if(Number == 0){
+    Console.WriteLine("it`s zero");
+    Number= result;
+    Console.WriteLine(result);
+} else{
+    Console.WriteLine("invaild value");
+}
+
+}
+ void GetGreeting(){
+    
+    Console.WriteLine("Enter your name");
+    string userName= Console.ReadLine();
+    Console.WriteLine("Hello" + userName);
+}
+
+
+}
+
+public class Car{
+    public string Make;
+    public string Model;
+    public int Year;
+    public string Color;
+    public Car(string Make, string Model, int Year,string Color) {
+        this.Make = Make;
+        this.Model = Model;
+        this.Year= Year;
+        this.Color = Color;
+
+    }
+
+
+
+
+ public void  DisplayDetails(){
+    Console.WriteLine("japan","lexus",2024,"pink");
+     Console.WriteLine("korea","kia",2023,"white");
+     Console.WriteLine("jerman","BMW",2022,"black");
+     Console.WriteLine("saudi","locid",2024,"green");
+      Console.WriteLine("US","ford",2019,"blue");
+
+ }
+ 
+ }
